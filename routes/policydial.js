86582e8040c7216d99d63272f1bf45049b8d935f
Pravin Bhat <>
2021-02-11 07:12:25 -0500

    Fixed constants error

--- routes/policydial.js
+++ routes/policydial.js
@@ -2,44 +2,21 @@ var express = require('express');
 var policydata = require('./data.js');
 var app = express.Router();
 
-// Summary
-const policySummary = {
-  "totalPoliciesCount": 21,
-  "triggeredPoliciesCount": 7,
-  "totalCategoryCount": 3,
-  "categoriesWithFinding": ["Privacy", "Protection"],
-  "policyScorePercent": 33
-};
-const policySummaryWeek = {
-  "totalPoliciesCount": 21,
-  "triggeredPoliciesCount": 5,
-  "totalCategoryCount": 3,
-  "categoriesWithFinding": ["Privacy", "Protection"],
-  "policyScorePercent": 23
-};
-const policySummaryMonth = {
-  "totalPoliciesCount": 21,
-  "triggeredPoliciesCount": 10,
-  "totalCategoryCount": 3,
-  "categoriesWithFinding": ["Privacy", "Protection"],
-  "policyScorePercent": 48
-};
-
 /* GET home page. */
 app.get('/', function (req, res, next) {
   res.render('index', { title: 'Policy Dial!', 
-  policySummary: policySummary,
-  policySummaryWeek: policySummaryWeek,
-  policySummaryMonth: policySummaryMonth
+  policySummary: policydata.policySummary,
+  policySummaryWeek: policydata.policySummaryWeek,
+  policySummaryMonth: policydata.policySummaryMonth
  });
 });
 
 // curl -X GET /policydial
 app.get('/data', (req, res) => {
   return res.send({ title: 'Policy Dial!', 
-  policySummary: policySummary,
-  policySummaryWeek: policySummaryWeek,
-  policySummaryMonth: policySummaryMonth
+  policySummary: policydata.policySummary,
+  policySummaryWeek: policydata.policySummaryWeek,
+  policySummaryMonth: policydata.policySummaryMonth
  });
 });
 
@@ -51,9 +28,9 @@ app.get('/policytypes', (req, res) => {
 // curl -X GET /policydial/policytypes/Privacy
 app.get('/policytypes/:policyType', (req, res) => {
   if (req.params.policyType.toLowerCase() === "privacy") {
-    return res.send(policyPrivacy);
+    return res.send(policydata.policyPrivacy);
   } else if (req.params.policyType.toLowerCase() === "protection") {
-    return res.send(policyProtection);
+    return res.send(policydata.policyProtection);
   }
 
   return res.send("");
@@ -62,19 +39,19 @@ app.get('/policytypes/:policyType', (req, res) => {
 // curl -X GET /policydial/ssn
 app.get('/:policyName', (req, res) => {
   if (req.params.policyName.toLowerCase() === "ssn") {
-    return res.send(privacySSNpolicy);
+    return res.send(policydata.privacySSNpolicy);
   } else if (req.params.policyName.toLowerCase() === "retentionexpired") {
-    return res.send(privacyRetentionExpiredPolicy);
+    return res.send(policydata.privacyRetentionExpiredPolicy);
   } else if (req.params.policyName.toLowerCase() === "medical") {
-    return res.send(privacyMedicalPolicy);
+    return res.send(policydata.privacyMedicalPolicy);
   } else if (req.params.policyName.toLowerCase() === "address") {
-    return res.send(privacyAddressPolicy);
+    return res.send(policydata.privacyAddressPolicy);
   } else if (req.params.policyName.toLowerCase() === "openaccess") {
-    return res.send(protectionOpenAccessPolicy);
+    return res.send(policydata.protectionOpenAccessPolicy);
   } else if (req.params.policyName.toLowerCase() === "token") {
-    return res.send(protectionTokenolicy);
+    return res.send(policydata.protectionTokenolicy);
   } else if (req.params.policyName.toLowerCase() === "duplicates") {
-    return res.send(protectionDuplicatesPolicy);
+    return res.send(policydata.protectionDuplicatesPolicy);
   }
 
   return res.send("");
