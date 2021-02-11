8e21bf6c9769d8f455e99835b171a37755e98e82
Pravin Bhat <>
2021-02-11 05:40:46 -0500

    Added UI dial & externalized constants & js

---
+++ public/javascripts/dial.js
@@ -0,0 +1,82 @@
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Now', policySummary.totalPoliciesCount]
+    ]);
+    var options = {
+        width: 700,
+        height: 300,
+        greenFrom: 0,
+        greenTo: (policySummary.totalPoliciesCount / 3),
+        yellowFrom: (policySummary.totalPoliciesCount / 3),
+        yellowTo: ((policySummary.totalPoliciesCount / 3) * 2),
+        redFrom: ((policySummary.totalPoliciesCount / 3) * 2),
+        redTo: policySummary.totalPoliciesCount,
+        minorTicks: policySummary.totalPoliciesCount,
+        majorTicks: ['0', policySummary.totalPoliciesCount],
+        max: policySummary.totalPoliciesCount,
+        animation: {
+            duration: 1000,
+            easing: 'out',
+        },
+    };
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+    data.setValue(0, 1, policySummary.triggeredPoliciesCount);
+    chart.draw(data, options);
+
+    var historyData = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Last Week', policySummaryWeek.totalPoliciesCount]
+    ]);
+    var historyWeekOptions = {
+        width: 400,
+        height: 150,
+        greenFrom: 0,
+        greenTo: (policySummaryWeek.totalPoliciesCount / 3),
+        yellowFrom: (policySummaryWeek.totalPoliciesCount / 3),
+        yellowTo: ((policySummaryWeek.totalPoliciesCount / 3) * 2),
+        redFrom: ((policySummaryWeek.totalPoliciesCount / 3) * 2),
+        redTo: policySummaryWeek.totalPoliciesCount,
+        minorTicks: policySummaryWeek.totalPoliciesCount,
+        majorTicks: ['0', policySummaryWeek.totalPoliciesCount],
+        max: policySummaryWeek.totalPoliciesCount,
+        animation: {
+            duration: 1000,
+            easing: 'out',
+        },
+    };
+    var historyChart = new google.visualization.Gauge(document.getElementById('week_chart_div'));
+    historyData.setValue(0, 1, policySummaryWeek.triggeredPoliciesCount);
+    historyChart.draw(historyData, historyWeekOptions);
+
+    var historyMonthData = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Last Month', policySummaryMonth.totalPoliciesCount]
+    ]);
+    var historyMonthOptions = {
+        width: 400,
+        height: 150,
+        greenFrom: 0,
+        greenTo: (policySummaryMonth.totalPoliciesCount / 3),
+        yellowFrom: (policySummaryMonth.totalPoliciesCount / 3),
+        yellowTo: ((policySummaryMonth.totalPoliciesCount / 3) * 2),
+        redFrom: ((policySummaryMonth.totalPoliciesCount / 3) * 2),
+        redTo: policySummaryMonth.totalPoliciesCount,
+        minorTicks: policySummaryMonth.totalPoliciesCount,
+        majorTicks: ['0', policySummaryMonth.totalPoliciesCount],
+        max: policySummaryMonth.totalPoliciesCount,
+        animation: {
+            duration: 1000,
+            easing: 'out',
+        },
+    };
+    var historyMonthChart = new google.visualization.Gauge(document.getElementById('month_chart_div'));
+    historyMonthData.setValue(0, 1, policySummaryMonth.triggeredPoliciesCount);
+    historyMonthChart.draw(historyMonthData, historyMonthOptions);
+
+    setInterval(function () {
+        data.setValue(0, 1, policySummary.triggeredPoliciesCount + Math.round(5 * Math.random()));
+        chart.draw(data, options);
+    }, 3000);
+}
--- routes/policydial.js
+++ routes/policydial.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var policydata = require('./data.js');
 var app = express.Router();
 
 // Summary
@@ -22,71 +23,6 @@ const policiesSummaryLastMonth = {
   "totalCategoryCount": 3,
   "categoriesWithFinding": ["Privacy", "Protection"],
   "policyScorePercent": 48
-};
-
-// Policy types
-const policyPrivacy = {
-  "policyType": "Privacy",
-  "totalPoliciesCount": 8,
-  "triggeredPolicies": ["SSN", "RetentionExpired", "Medical", "Address"],
-  "policyScorePercent": 50
-};
-const policyProtection = {
-  "policyType": "Protection",
-  "totalPoliciesCount": 7,
-  "triggeredPolicies": ["OpenAccess", "Token", "Duplicates"],
-  "policyScorePercent": 43
-};
-
-// Individual policy finding details
-const privacySSNpolicy = {
-  "name": "SSN",
-  "policyType": "Privacy",
-  "triggerThreshold": 1,
-  "noOfFindings": 12,
-  "lastTriggerTime": "12/14/2020, 10:42:04 AM"
-};
-const privacyRetentionExpiredPolicy = {
-  "name": "RetentionExpired",
-  "policyType": "Privacy",
-  "triggerThreshold": 10,
-  "noOfFindings": 123,
-  "lastTriggerTime": "12/14/2020, 10:42:04 AM"
-};
-const privacyMedicalPolicy = {
-  "name": "Medical",
-  "policyType": "Privacy",
-  "triggerThreshold": 10,
-  "noOfFindings": 16,
-  "lastTriggerTime": "12/14/2020, 10:42:04 AM"
-};
-const privacyAddressPolicy = {
-  "name": "Address",
-  "policyType": "Privacy",
-  "triggerThreshold": 100,
-  "noOfFindings": 345,
-  "lastTriggerTime": "12/14/2020, 10:42:04 AM"
-};
-const protectionOpenAccessPolicy = {
-  "name": "OpenAccess",
-  "policyType": "Protection",
-  "triggerThreshold": 50,
-  "noOfFindings": 145,
-  "lastTriggerTime": "12/14/2020, 10:42:04 AM"
-};
-const protectionTokenolicy = {
-  "name": "Token",
-  "policyType": "Protection",
-  "triggerThreshold": 100,
-  "noOfFindings": 1234,
-  "lastTriggerTime": "12/14/2020, 10:42:04 AM"
-};
-const protectionDuplicatesPolicy = {
-  "name": "Duplicates",
-  "policyType": "Protection",
-  "triggerThreshold": 1000,
-  "noOfFindings": 2234,
-  "lastTriggerTime": "12/14/2020, 10:42:04 AM"
 };
 
 // curl -X GET /policydial
